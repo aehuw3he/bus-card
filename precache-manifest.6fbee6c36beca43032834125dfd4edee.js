e7f889d63bc4f4725af96e931317d66f70c20695
ajeiafeiafj13r <faefjeijf22r>
2019-11-21 22:48:41 +0800

    Updates

---
+++ precache-manifest.6fbee6c36beca43032834125dfd4edee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2d7427073fc08602529cb5369fdbb45",
+    "url": "/bus-card/index.html"
+  },
+  {
+    "revision": "8874397be2cb7c6daba8",
+    "url": "/bus-card/static/css/main.9c266401.chunk.css"
+  },
+  {
+    "revision": "c18dc87f3a35a5f6098c",
+    "url": "/bus-card/static/js/2.c5c7b94c.chunk.js"
+  },
+  {
+    "revision": "8874397be2cb7c6daba8",
+    "url": "/bus-card/static/js/main.be56354b.chunk.js"
+  },
+  {
+    "revision": "f438d8af9dcbf5cb34c7",
+    "url": "/bus-card/static/js/runtime-main.3b8a9ae2.js"
+  },
+  {
+    "revision": "2260b52a9798479eb840deff46767be0",
+    "url": "/bus-card/static/media/logo.2260b52a.png"
+  }
+]);
